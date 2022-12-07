5d6324f21e4bdb9abb566bd7225fa42346c37c55
7777766666 <112616064+7777766666@users.noreply.github.com>
2022-12-07 04:14:39 +0300

    Add files via upload
    Ещё разок эту задачу

---
+++ src/main/java/com/example/springboot3/entity/Role.java
@@ -0,0 +1,61 @@
+package com.example.springboot3.entity;
+
+import com.sun.istack.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
